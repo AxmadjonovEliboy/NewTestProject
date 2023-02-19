0717b41d0e0cf7b6a6235f6ba2f8501c1f33468c
jarvis <axmadjonoveliboy@gmail.com>
2023-02-20 00:46:33 +0500

    UserService class 1

--- src/model/User.java
+++ src/model/User.java
@@ -4,4 +4,5 @@ package model;
  * @author Jarvis on Mon 00:16. 20/02/23
  */
 public class User {
+
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,7 +1,5 @@
 package service;
 
 public class UserService {
-    public static void main(String[] args) {
-        System.out.println("salom");
-    }
+
 }
